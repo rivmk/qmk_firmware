08443678acb7bd3a9edcfb677a7307708feadbe1
nmyeti <chriveron@gmail.com>
2021-05-18 09:05:24 +0900

    rev3

---
+++ keyboards/yynmt/acperience12/keymaps/artsey/config.h
@@ -0,0 +1,32 @@
+/* Copyright 2021 yynmt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+
+
+#define COMBO_TERM 200
+#define ONESHOT_TIMEOUT 5000 
+
+#define MOUSEKEY_INTERVAL 12
+#define MOUSEKEY_MAX_SPEED 6
+#define MOUSEKEY_TIME_TO_MAX 50
+#define MOUSEKEY_DELAY 20
+
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
---
+++ keyboards/yynmt/acperience12/rev1/rules.mk
@@ -0,0 +1,7 @@
+# MCU name
+MCU = atmega32u2
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# COMBO_ENABLE = yes
