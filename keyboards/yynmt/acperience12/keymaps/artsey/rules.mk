43b927fbe38e337fc5753f45ff75424e6d325786
nmyeti <chriveron@gmail.com>
2021-05-22 18:10:43 +0900

    added ferris original keymap

--- keyboards/yynmt/acperience12/keymaps/artsey/rules.mk
+++ keyboards/yynmt/acperience12/keymaps/artsey/rules.mk
@@ -8,8 +8,7 @@ EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = no	# Console for debug
 COMMAND_ENABLE = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	    # USB Nkey Rollover
-
+# FORCE_NKRO = yes
 COMBO_ENABLE = yes
 
 # LEADER_ENABLE = yes
